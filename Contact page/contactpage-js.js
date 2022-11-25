5a6ce8625204cd0c22222478ee54862eccdbd64c
Jasper Luik <jasperlu@ut.ee>
2022-11-25 16:50:47 +0200

    lisasin töötava contact formi

---
+++ Contact page/contactpage-js.js
@@ -0,0 +1,10 @@
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onmouseover
+function bigImg(x) {
+    x.style.height = "64px";
+    x.style.width = "64px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "32px";
+    x.style.width = "32px";
+  }
